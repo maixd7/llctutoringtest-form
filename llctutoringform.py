8548ac378f72b3679790d511ed9f3bbf1c2e1006
maixd7 <122345406+maixd7@users.noreply.github.com>
2024-10-11 12:00:07 -0700

    Update llctutoringform.py

--- llctutoringform.py
+++ llctutoringform.py
@@ -2,10 +2,14 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 
+st.cache_data.clear()
 conn = st.connection("gsheets", type=GSheetsConnection)
 existing_data = conn.read(worksheet="Lesson", usecols=list(range(6)))
+student_data = conn.read(worksheet="Client", usecols=list(range(7)))
+package_data = conn.read(worksheet="Lesson Package", usecols=list(range(4)))
 existing_data = existing_data.dropna(how="all")
 max_id = existing_data['ID'].max()
+
 
 with st.form(key="llctutoring_form"):
     studentName = st.text_input(label="Student Name")
@@ -13,9 +17,7 @@ with st.form(key="llctutoring_form"):
     subject = st.text_input(label="Subject")
     lessonDate = st.date_input(label="Lesson Date")
     notes = st.text_input(label="Notes:")
-
     submit_button = st.form_submit_button(label="Submit")
-
     if submit_button:
         st.write("You have submitted")
         form_data = pd.DataFrame(
@@ -30,7 +32,17 @@ with st.form(key="llctutoring_form"):
                 }
             ]
         )
+        # Identifying lesson package id
+        student_row = student_data[student_data["Student Name"] == studentName]
+        lesson_package_id = student_row.iloc[0]['Lesson Package id']
+        # Updating lesson package
+        package_row = package_data[package_data["id"] == lesson_package_id]
+        current_count = package_row.iloc[0]['Current Lesson Count']
+        new_count = current_count+1
+        package_data.loc[package_data["id"] == lesson_package_id, "Current Lesson Count"] = new_count
+        #Update spreadsheet
         update_df = pd.concat([existing_data, form_data])
+        conn.update(worksheet="Lesson Package", data=package_data)
         conn.update(worksheet = "Lesson", data=update_df)
         st.cache_data.clear()
         
