9e5dc320dd724d9c6dc78055424e7a72c0daad91
maixd7 <coolmichael08@gmail.com>
2024-10-22 02:21:52 -0700

    update

--- llctutoringform.py
+++ llctutoringform.py
@@ -8,31 +8,36 @@ existing_data = existing_data.dropna(how="all")
 max_id = existing_data['ID'].max()
 
 with st.form(key="llctutoring_form"):
-    studentName = st.text_input(label="Student Name")
-    tutorName = st.text_input(label="Tutor Name")
-    subject = st.text_input(label="Subject")
-    lessonDate = st.date_input(label="Lesson Date")
-    notes = st.text_input(label="Notes:")
+    studentName = st.text_input(label="Student Name*", )
+    tutorName = st.text_input(label="Tutor Name*")
+    subject = st.text_input(label="Subject*")
+    lessonDate = st.date_input(label="Lesson Date*")
+    notes = st.text_input(label="Notes:*")
 
     submit_button = st.form_submit_button(label="Submit")
+    st.markdown("**required*")
 
     if submit_button:
-        st.write("You have submitted")
-        form_data = pd.DataFrame(
-            [
-                {
-                    "ID": max_id+1,
-                    "Student Name": studentName,
-                    "Tutor Name": tutorName,
-                    "Subject": subject,
-                    "Lesson Date": lessonDate,
-                    "Notes": notes,
-                }
-            ]
-        )
-        update_df = pd.concat([existing_data, form_data])
-        conn.update(worksheet = "Lesson", data=update_df)
-        st.cache_data.clear()
-        
+        if not studentName or tutorName or subject or lessonDate or notes:
+            st.warning("Ensure all mandatory fields are filled.")
+            st.stop()
+        else:
+            st.write("You have submitted")
+            form_data = pd.DataFrame(
+                [
+                    {
+                        "ID": max_id+1,
+                        "Student Name": studentName,
+                        "Tutor Name": tutorName,
+                        "Subject": subject,
+                        "Lesson Date": lessonDate,
+                        "Notes": notes,
+                    }
+                ]
+            )
+            update_df = pd.concat([existing_data, form_data])
+            conn.update(worksheet = "Lesson", data=update_df)
+            st.cache_data.clear()
+            
         
         
