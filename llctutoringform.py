59f12e119c8daf0004259a324fda239c8d454836
maixd7 <coolmichael08@gmail.com>
2024-12-07 18:46:39 -0800

    updated error handling

--- llctutoringform.py
+++ llctutoringform.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+from datetime import datetime
 import pandas as pd
 
 hide_st_style = """
@@ -9,6 +10,7 @@ hide_st_style = """
             header {visibility: hidden;}
             </style>
             """
+current_date = datetime.now().date()
 st.markdown(hide_st_style, unsafe_allow_html=True)
 st.cache_data.clear()
 conn = st.connection("gsheets", type=GSheetsConnection)
@@ -19,49 +21,52 @@ existing_data = existing_data.dropna(how="all")
 max_id = existing_data['ID'].max()
 
 
-with st.form(key="llctutoring_form"):
+with st.form(key="llctutoring_form", clear_on_submit=True):
     studentName = st.text_input(label="Student Name*", )
     tutorName = st.text_input(label="Tutor Name*")
     subject = st.text_input(label="Subject*")
-    lessonDate = st.date_input(label="Lesson Date*")
+    lessonDate = st.date_input(label="Lesson Date*", value=current_date)
     notes = st.text_input(label="Notes:*")
 
     submit_button = st.form_submit_button(label="Submit")
     st.markdown("**required*")
 
     if submit_button:
-        if not (studentName or tutorName or subject or lessonDate or notes):
-            st.warning("Ensure all mandatory fields are filled.")
+        if not studentName or not tutorName or not subject or not lessonDate or not notes:
+            st.error("Ensure all mandatory fields are filled.")
             st.stop()
         else:
-            st.write("You have submitted")
-            # Identifying lesson package id
-            student_row = student_data[student_data["Student Name"] == studentName.title().strip()]
-            lesson_package_id = student_row.iloc[0]['Lesson Package id']
-            # Updating lesson package
-            package_row = package_data[package_data["id"] == lesson_package_id]
-            current_count = package_row.iloc[0]['Current Lesson Count']
-            new_count = current_count+1
-            package_data.loc[package_data["id"] == lesson_package_id, "Current Lesson Count"] = new_count
-            form_data = pd.DataFrame(
-                [
-                    {
-                        "ID": max_id+1,
-                        "Student Name": studentName.title().strip(),
-                        "Tutor Name": tutorName.title().strip(),
-                        "Subject": subject,
-                        "Lesson Date": lessonDate,
-                        "Notes": notes,
-                        "Lesson Number": new_count
-                    }
-                ]
-            )
-            #Update spreadsheet
-            update_df = pd.concat([existing_data, form_data])
-            conn.update(worksheet = "Lesson", data=update_df)
-            conn.update(worksheet="Lesson Package", data=package_data)
-            conn.update(worksheet = "Lesson", data=update_df)
-            st.cache_data.clear()
+            try:
+                st.write("You have submitted")
+                # Identifying lesson package id
+                student_row = student_data[student_data["Student Name"] == studentName.title().strip()]
+                lesson_package_id = student_row.iloc[0]['Lesson Package id']
+                # Updating lesson package
+                package_row = package_data[package_data["id"] == lesson_package_id]
+                current_count = package_row.iloc[0]['Current Lesson Count']
+                new_count = current_count+1
+                package_data.loc[package_data["id"] == lesson_package_id, "Current Lesson Count"] = new_count
+                form_data = pd.DataFrame(
+                    [
+                        {
+                            "ID": max_id+1,
+                            "Student Name": studentName.title().strip(),
+                            "Tutor Name": tutorName.title().strip(),
+                            "Subject": subject,
+                            "Lesson Date": lessonDate,
+                            "Notes": notes,
+                            "Lesson Number": new_count
+                        }
+                    ]
+                )
+                #Update spreadsheet
+                update_df = pd.concat([existing_data, form_data])
+                conn.update(worksheet = "Lesson", data=update_df)
+                conn.update(worksheet="Lesson Package", data=package_data)
+                conn.update(worksheet = "Lesson", data=update_df)
+                st.cache_data.clear()
+            except IndexError:
+                st.error("Invalid student name: " + studentName + " not found.")
             
         
         
